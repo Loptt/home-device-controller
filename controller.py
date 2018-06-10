8e049963eb1f3ce66a4970169db4f8765d097afd
Loptt <carlos.deg02@hotmail.com>
2018-06-10 15:53:56 -0500

    Added class irrigation, date, and time

---
+++ controller.py
@@ -0,0 +1,65 @@
+import time 
+
+class Date:
+
+	year = 0
+	month = 0
+	day = 0
+
+	def __init__(self, year = 0, month = 0, day = 0):
+		self.day = day
+		self.month = month
+		self.year = year
+
+class Time:
+
+	hours = 0
+	minutes = 0
+
+	def __init__(self, hours = 0, minutes = 0):
+		self.hours = hours
+		self.minutes = minutes
+
+
+class IrrigationSystem:
+
+	current_date = Date()
+	current_time = Time()
+
+	def __init__(self):
+		pass
+
+	def get_date_and_time(self):
+		get_hour = time.strftime("%H")
+		get_minute = time.strftime("%M")
+		self.current_time = Time(get_hour, get_minute)
+
+		get_day = time.strftime("%j")
+		get_month = time.strftime("%m")
+		get_year = time.strftime("%y") 
+		self.current_date = Date(get_year, get_month, get_day)
+
+	def run(self):
+
+		while True:
+
+			self.get_date_and_time()
+
+			if self.current_time.hours == 22 and self.current_time.minutes == 30:
+				# turn ON first irrigation
+				# turn OFF second irrigation
+				pass
+
+			elif self.current_time.hours == 22 and self.current_time.minutes == 40:
+				# turn OFF first irrigation
+				# turn ON second irrigation
+				pass
+
+			else:
+				# turn OFF fist irrigation
+				# turn OFF second irrigation
+				pass
+
+irrigation_system = IrrigationSystem()
+
+irrigation_system.run()
