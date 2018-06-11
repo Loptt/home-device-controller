8b505be6c7cb0d537baafa4ec7e07658730b829a
Carlos Estrada <carlos.deg02@hotmail.com>
2018-06-11 15:51:20 -0500

    Added client and debugged server

--- controller.py
+++ controller.py
@@ -1,75 +1,77 @@
-import time 
+import time
 import server
 import threading
 
+
 class Date:
 
-	year = 0
-	month = 0
-	day = 0
+    year = 0
+    month = 0
+    day = 0
 
-	def __init__(self, year = 0, month = 0, day = 0):
-		self.day = day
-		self.month = month
-		self.year = year
+    def __init__(self, year=0, month=0, day=0):
+        self.day = day
+        self.month = month
+        self.year = year
+
 
 class Time:
 
-	hours = 0
-	minutes = 0
+    hours = 0
+    minutes = 0
 
-	def __init__(self, hours = 0, minutes = 0):
-		self.hours = hours
-		self.minutes = minutes
+    def __init__(self, hours=0, minutes=0):
+        self.hours = hours
+        self.minutes = minutes
 
 
 class IrrigationSystem:
 
-	current_date = Date()
-	current_time = Time()
+    current_date = Date()
+    current_time = Time()
 
-	serv = server.Server()
+    serv = server.Server()
 
-	def __init__(self):
-		server_thread = threading.Thread(target=self.serv.run())
-		server_thread.daemon = True
-		server_thread.start()
+    def __init__(self):
+        server_thread = threading.Thread(target=self.serv.run())
+        server_thread.daemon = True
+        server_thread.start()
 
-	def get_date_and_time(self):
-		get_hour = int(time.strftime("%H"))
-		get_minute = int(time.strftime("%M"))
-		self.current_time = Time(get_hour, get_minute)
+    def get_date_and_time(self):
+        get_hour = int(time.strftime("%H"))
+        get_minute = int(time.strftime("%M"))
+        self.current_time = Time(get_hour, get_minute)
 
-		get_day = int(time.strftime("%j"))
-		get_month = int(time.strftime("%m"))
-		get_year = int(time.strftime("%y")) 
-		self.current_date = Date(get_year, get_month, get_day)
+        get_day = int(time.strftime("%j"))
+        get_month = int(time.strftime("%m"))
+        get_year = int(time.strftime("%y"))
+        self.current_date = Date(get_year, get_month, get_day)
 
-	def run(self):
+    def run(self):
 
-		while True:
+        while True:
 
-			self.get_date_and_time()
-			command = self.serv.get_message()
+            self.get_date_and_time()
+            command = self.serv.get_message()
 
-			if command != "":
-				print(command)
+            if command != "":
+                print("Command", command)
 
-			if self.current_time.hours == 16:
-				# turn ON first irrigation
-				# turn OFF second irrigation
-				print("Inicio riego 1")
+            if self.current_time.hours == 22 and self.current_time.minutes == 30:
+                # turn ON first irrigation
+                # turn OFF second irrigation
+                print("Inicio riego 1")
 
-			elif self.current_time.hours == 22 and self.current_time.minutes == 40:
-				# turn OFF first irrigation
-				# turn ON second irrigation
-				pass
+            elif self.current_time.hours == 22 and self.current_time.minutes == 40:
+                # turn OFF first irrigation
+                # turn ON second irrigation
+                pass
 
-			else:
-				# turn OFF fist irrigation
-				# turn OFF second irrigation
-				#print("Todo apagado")
-				pass
+            else:
+                # turn OFF fist irrigation
+                # turn OFF second irrigation
+                #print("Todo apagado")
+                pass
 
 
 irrigation_system = IrrigationSystem()
--- server.py
+++ server.py
@@ -2,70 +2,73 @@ import socket
 import sys
 import threading
 
+
 class Server:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	connections = []
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
 
-	host = ""
-	port = 5000
-	server_address = (host, port)
-	command = ""
+    host = ""
+    port = 5000
+    server_address = (host, port)
+    command = ""
 
-	def __init__(self):
-		try:
-			self.s.bind(self.server_address)
-			self.s.listen(1)
+    def __init__(self):
+        try:
+            self.s.bind(self.server_address)
+            self.s.listen(1)
 
-		except socket.error as e:
-			print(str(e))
+        except socket.error as e:
+            print(str(e))
 
-	def get_message(self):
+    def get_message(self):
 
-		message = self.command
-		self.command = ""
+        message = self.command
+        self.command = ""
 
-		return message
+        return message
 
-	def handle_connection(self, conn, conn_address):
-		conn.send(str.encode("Connected to Raspberry PI\n"))
+    def handle_connection(self, conn, conn_address):
+        conn.send(str.encode("Connected to Raspberry PI\n"))
 
-		while True:
+        while True:
 
-			data = conn.recv(1024).decode()   
+            data = conn.recv(1024)
 
-			if not data:
-				print(str(conn_address[0]) + ":" + str(conn_address[1]) + " dissconnected")
-				conn.close()
-				self.connections.remove(conn)
-				break
+            if not data:
+                print(str(conn_address[0]) + ":" +
+                      str(conn_address[1]) + " dissconnected")
+                conn.close()
+                self.connections.remove(conn)
+                break
 
-			self.command = data.decode()
+            self.command = data.decode()
 
-	def send_message(self):
+    def send_message(self):
 
-		while True:
+        while True:
 
-			print("Input message to clients:")
+            print("Input message to clients:")
 
-			try:
-				message = ("Server says: " + input()).encode()
-			except:
-				print("Data input error")
+            try:
+                message = ("Server says: " + input()).encode()
+            except:
+                print("Data input error")
 
-			for connection in self.connections:
-				connection.sendall(message)
+            for connection in self.connections:
+                connection.sendall(message)
 
+    def run(self):
+        print("Initializing server...")
+        print("Waiting for connections...\n")
 
-	def run(self):
-		print("Initializing server...")
-		print("Waiting for connections...\n")
+        while True:
+            conn, conn_address = self.s.accept()
+            thread = threading.Thread(
+                target=self.handle_connection, args=(conn, conn_address))
+            thread.daemon = True
+            thread.start()
 
-		while True:
-			conn, conn_address = self.s.accept()
-			thread = threading.Thread(target=self.handle_connection, args=(conn,conn_address))
-			thread.daemon = True
-			thread.start()
+            self.connections.append(conn)
 
-			self.connections.append(conn)
-
-			print(str(conn_address[0]) + ":" + str(conn_address[1]) + " connected")
+            print(str(conn_address[0]) + ":" +
+                  str(conn_address[1]) + " connected")
