5e50001280eb0a3393b86b30ef7bed8a9a3dc6c9
Carlos Estrada <carlos.deg02@hotmail.com>
2018-12-12 22:26:29 -0600

    Updated files

--- JavaServer/src/com/comolaestaviendo/charles/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.comolaestaviendo.charles;
-
-public class Constants {
-    public static final String CLOSED_CONNECTION = "closed_connection";
-    public static final String LOGIN_NAME = "login_name";
-}
